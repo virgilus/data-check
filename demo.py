46bdc2688ba31395009e10f725df046529c71f4d
virgilus <virgile@jazzdata.fr>
2024-07-22 11:50:57 +0200

    adds a comment

--- demo.py
+++ demo.py
@@ -4,4 +4,4 @@ from math import pi
 
 def circle_area(radius):
     """Returns the area of the circle of given radius"""
-    return pi * radius**2 if radius > 0 else 0
+    return pi * radius**2 if radius > 0 else 0 # shorter way

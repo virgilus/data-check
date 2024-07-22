ab5600f902031a5abb8d4587f08b0839ca448036
virgilus <virgile@jazzdata.fr>
2024-07-22 11:46:46 +0200

    first commit

---
+++ demo.py
@@ -0,0 +1,7 @@
+"""Dummy challenge for Kitt Demo"""
+from math import pi
+
+
+def circle_area(radius):
+    """Returns the area of the circle of given radius"""
+    return pi * radius**2 if radius > 0 else 0
